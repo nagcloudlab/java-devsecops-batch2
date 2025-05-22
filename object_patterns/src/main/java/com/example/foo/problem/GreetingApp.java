cc569ea03ed6dce0c943614f4fd0f97fb4438249
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-22 18:43:40 +0530

    day-4 commit

--- object_patterns/src/main/java/com/example/foo/problem/GreetingApp.java
+++ object_patterns/src/main/java/com/example/foo/problem/GreetingApp.java
@@ -1,2 +1,47 @@
-package com.example.foo.problem;public class GreetingApp {
+package com.example.foo.problem;
+
+
+/*
+    design issues
+
+    -> code tangling / coupling
+    -> code scatting / duplication
+
+ */
+
+class Greeting {
+
+    // dev-1:
+    public void hello() {
+        System.out.println("Hello");
+        System.out.println("😀");
+    }
+
+    public void helloWithoutEmoji() {
+        System.out.println("Hello");
+    }
+
+    // Me
+    public void hi() {
+        System.out.println("Hi");
+        System.out.println("😀");
+    }
+
+    public void hey() {
+        System.out.println("Hey");
+        System.out.println("😀");
+    }
+
+}
+
+
+public class GreetingApp {
+    public static void main(String[] args) {
+
+        Greeting greeting = new Greeting();
+        greeting.hello();
+        greeting.hi();
+        greeting.hey();
+
+    }
 }
