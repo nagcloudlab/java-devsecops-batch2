8e462add23ad4aea8d349adee2d431fb927d6cdf
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-26 16:34:23 +0530

    Collection intro & exercise

---
+++ collections/src/main/java/com/example/Exercise1.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.example.model.Dish;
+
+import java.util.List;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        List<Dish> menu=Dish.menu;
+
+        // Get low calorie ( < 400 calories) dish's names in new List ( sorted by cal )
+    }
+
+}
