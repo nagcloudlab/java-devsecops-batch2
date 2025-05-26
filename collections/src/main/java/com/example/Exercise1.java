f8ca03e27636576fdc415d6d2f1704b82ced4c56
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-26 17:13:13 +0530

    solution to exercise

--- collections/src/main/java/com/example/Exercise1.java
+++ collections/src/main/java/com/example/Exercise1.java
@@ -2,15 +2,42 @@ package com.example;
 
 import com.example.model.Dish;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise1 {
 
     public static void main(String[] args) {
 
-        List<Dish> menu=Dish.menu;
-
+        List<Dish> menu = Dish.menu;
         // Get low calorie ( < 400 calories) dish's names in new List ( sorted by cal )
+        // step-1 : filter dish to new list
+        List<Dish> filteredMenu = new ArrayList<>();
+        for (Dish dish : menu) {
+            if (dish.getCalories() < 400) {
+                filteredMenu.add(dish);
+            }
+        }
+        // step-2 : sort by cal
+        class CalComparator implements Comparator<Dish> {
+            @Override
+            public int compare(Dish o1, Dish o2) {
+                return Integer.compare(o1.getCalories(), o2.getCalories());
+            }
+        }
+        Collections.sort(filteredMenu, new CalComparator());
+        // step-3 : extract dish name into new list
+        List<String> dishNames = new ArrayList<>();
+        for (Dish dish : filteredMenu) {
+            dishNames.add(dish.getName());
+        }
+
+        System.out.println(dishNames);
+
+
     }
 
 }
