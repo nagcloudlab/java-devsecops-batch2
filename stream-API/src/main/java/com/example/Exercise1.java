6943b2653f2889ce909b17cb92c0b43a90976933
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-28 17:00:01 +0530

    streams api exercise solution

--- stream-API/src/main/java/com/example/Exercise1.java
+++ stream-API/src/main/java/com/example/Exercise1.java
@@ -5,6 +5,7 @@ import com.example.model.Transaction;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Exercise1 {
     public static void main(String[] args) {
@@ -30,5 +31,50 @@ public class Exercise1 {
         // Query 5: Are there any trader based in Milan?
         // Query 6: Update all transactions so that the traders from Milan are set to Cambridge
         //--------------------------------------------------------
+
+
+        // Query 1: Find all transactions from year 2011 and sort them by value (small to high).
+        transactions.stream()
+                .filter(t -> t.getYear() == 2011)
+                .sorted((t1, t2) -> Integer.compare(t2.getValue(), t1.getValue()))
+                .forEach(System.out::println);
+
+        System.out.println();
+        // Query 2: What are all the unique cities where the traders work?
+        transactions
+                .stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getCity)
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println();
+        // Query 3: Find all traders from Cambridge and sort them by name.
+        transactions
+                .stream()
+                .map(t -> t.getTrader())
+                .filter(t -> t.getCity().equals("Cambridge"))
+                .distinct()
+                .sorted((t1, t2) -> t1.getName().compareTo(t2.getName()))
+                .forEach(System.out::println);
+
+        System.out.println();
+        // Query 5: Are there any trader based in Milan?
+
+        boolean areMilanTradersExists = transactions
+                .stream().anyMatch(t -> t.getTrader().getCity().equals("Milan"));
+        System.out.println(areMilanTradersExists);
+
+        // Query 6: Update all transactions so that the traders from Milan are set to Cambridge
+        transactions
+                .stream()
+                .filter(t -> t.getTrader().getCity().equals("Milan"))
+                .forEach(t -> {
+                    t.getTrader().setCity("Cambridge");
+                });
+
+        transactions
+                .forEach(System.out::println);
+
+
     }
 }
